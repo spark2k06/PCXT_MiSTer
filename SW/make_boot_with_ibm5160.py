e07bbc16fc5feb5624ed7e6a5f444e86a0236bfa
jordism <jordism@tid.es>
2022-07-06 13:00:59 +0200

    feat(ibm-5160-1982): update to use 1982 bios

--- SW/make_boot_with_ibm5160.py
+++ SW/make_boot_with_ibm5160.py
@@ -3,34 +3,33 @@ import glob
 import zipfile
 import requests
 
-URL = "http://www.minuszerodegrees.net/bios/BIOS_5160_09MAY86.zip"
-response = requests.get(URL)
-open("ibm5160.zip", "wb").write(response.content)
+if __name__ == "__main__":
+    URL = "http://minuszerodegrees.net/bios/BIOS_5160_08NOV82.zip"
+    response = requests.get(URL)
+    open("ibm5160.zip", "wb").write(response.content)
 
-with zipfile.ZipFile("ibm5160.zip", 'r') as zip_ref:
-    zip_ref.extractall()
+    with zipfile.ZipFile("ibm5160.zip", 'r') as zip_ref:
+        zip_ref.extractall()
 
-try:
-    os.remove("ibm5160.zip")
-except:
-    print("Error while deleting file : ibm5160.zip")
+        try:
+            os.remove("ibm5160.zip")
+        except:
+            print("Error while deleting file : ibm5160.zip")
 
-rom_filename = "boot.rom"
-ibm5160_basename = "BIOS_5160_09MAY86_"
+            rom_filename = "boot.rom"
+            ibm5160_basename = "BIOS_5160_08NOV82_"
 
-with open(rom_filename, "wb") as romf, open(ibm5160_basename + "U19_62X0819_68X4370_27256_F000.bin", "rb") as f:
-     romf.write(f.read())
+            with open(rom_filename, "wb") as romf, open(ibm5160_basename + "U19_5000027_27256.bin", "rb") as f:
+                romf.write(f.read())
 
-with open(rom_filename, "ab") as romf, open(ibm5160_basename + "U18_59X7268_62X0890_27256_F800.bin", "rb") as f:
-     romf.write(f.read())
+                with open(rom_filename, "ab") as romf, open(ibm5160_basename + "U18_1501512.bin", "rb") as f:
+                    romf.write(f.read())
 
-fileList = glob.glob(ibm5160_basename + "*.bin")
+                    fileList = glob.glob(ibm5160_basename + "*.bin")
 
-for filePath in fileList:
-    try:
-        os.remove(filePath)
-    except:
-        print("Error while deleting file : ", filePath)
-        
+                    for filePath in fileList:
+                        try:
+                            os.remove(filePath)
+                        except:
+                            print("Error while deleting file : ", filePath)
 
-    
