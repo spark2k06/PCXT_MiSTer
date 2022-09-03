ebde6e5c792221af96a1cecdabc2ab09f1a83093
Aitor Gómez <spark2k06@gmail.com>
2022-09-03 08:13:40 +0200

    Overscan colour fix in Tandy Mode
    Many games in Tandy mode have a white overscan, when on original hardware or emulators it appears black.
    
    I don't know if the solution is correct, but I have forced it to be black when the overscan register is bright white (0xF).

--- rtl/video/cga_attrib.v
+++ rtl/video/cga_attrib.v
@@ -88,7 +88,7 @@ module cga_attrib(
                 2'b00: pix_out <= att_fg; // Text foreground
                 2'b01: pix_out <= att_bg; // Text background
                 2'b10: pix_out <= active_area; // Graphics
-                2'b11: pix_out <= cga_color_reg[3:0]; // Overscan color
+                2'b11: pix_out <= (tandy_16_mode & (cga_color_reg[3:0] == 4'b1111)) ? 4'b0 : cga_color_reg[3:0]; // Overscan color
             endcase
         end
     end
